e4ce0a8c8c325971d66899aa328aecadfa22fe06
Sparky-168 <sparkyrogers168@gmail.com>
2024-10-30 11:06:45 -0400

    Create symtab.c

---
+++ symtab.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"//not sure if this is needed here, but leaving it in case
+#include "id_attrs.h"
+#include "ast.h"
+#include "symtab.h"
+#include "scope_check.h"
+#include "utilities.h"
+#include "id_use.h"
